f21ef391527e5fb43d64da7cabf4f9e79daf074f
Derek Wong <djwong23@gmail.com>
2020-12-09 18:47:45 -0500

    potentially working server, must send inputs with ctrl d

--- Asst3.c
+++ Asst3.c
@@ -1,18 +1,24 @@
+#include <ctype.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define MAX 80
+
 #define BACKLOG 5
 
-
+char *checkMessage(int stage, char *message, char *setUpLine, char *punchLine, char *adsLine);
+char *findError(int stage, char *message, char *expected, int expectedLen);
+int numDigits(int len);
+char *lengthAsString(int len);
+int readXBytes(int socketFileDesc, char *buff, int x);
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		printf("Usage: %s [port]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
+	//socket setup; socket contained in socketFileDesc
 	char *port = argv[1];
 	struct addrinfo hint, *address_list, *addr;
 	int error, socketFileDesc, connectionFileDesc;
@@ -48,6 +54,7 @@ int main(int argc, char **argv) {
 	}
 	freeaddrinfo(address_list);
 	printf("Waiting for connection\n");
+	//accepts connection, opens new socket in connectionFileDesc to allow communication
 	for (;;) {
 
 		connectionFileDesc = accept(socketFileDesc, socketAddress, &addrlen);
@@ -56,184 +63,305 @@ int main(int argc, char **argv) {
 			perror("accept");
 			continue;
 		}
-		char buff[MAX];
+		int size = 100;
+		char *buff = malloc(sizeof(char) * size);
+		if (buff == NULL) {
+			printf("Malloc error.\n");
+			return 1;
+		}
+		int finished = 0;
 		//currently reads until CTRL-C given, sends input to checkMessage
-		for (int i = 1; i <= 4; i++) {
-			bzero(buff, MAX);
+		for (int i = 1; i <= 4 && !finished; i++) {
+			bzero(buff, size);
 			switch (i) {
 				case 1: {
 					char intro[] = "Knock, knock.\n";
 					write(connectionFileDesc, intro, sizeof(intro));
-				} break;
+					break;
+				}
 				case 2: {
 					write(connectionFileDesc, setUpLine, sizeof(setUpLine));
-				} break;
+					break;
+				}
 				case 3: {
 					write(connectionFileDesc, punchLine, sizeof(punchLine));
-				} break;
+					break;
+				}
 				case 4: {
-					return 0;
+					finished = 1;
+					continue;
 				}
 				default: {
 					return 1;
 				}
 			}
 
-			int nread = read(connectionFileDesc, buff, MAX);
-			buff[nread] = '\0';
-			if (nread == 0) {
-				printf("Got EOF\n");
-				return 0;
+			//readXBytes repeatedly reads until the bytes requested are read
+			//returns 1 if the bytes requested cannot be read
+			if (readXBytes(connectionFileDesc, buff, 4) == 1) {
+				printf("Early termination of client.\n");
+				break;
 			}
-			// checkMessage(i, buff);
+			buff[4] = '\0';
+			if (strcmp("REG|", buff) == 0) {
+				int k = 0;
+				//need to read in all digits of the length
+				while (1) {
+					if (4 + k == size) {
+						size *= 2;
+						buff = realloc(buff, size);
+						if (buff == NULL) {
+							printf("Malloc error.\n");
+							return 1;
+						}
+					}
+					if (read(connectionFileDesc, &buff[4 + k], 1) == 0) {
+						printf("Early termination of client. \n");
+						finished = 1;
+						break;
+					}
+					printf("Read in %c\n", buff[4+k]);
+					if (!isdigit(buff[4 + k]) && buff[4 + k] != '|') {
+						buff[4 + k + 1] = '\0';
+						printf("%s is the buff\n", buff);
+						printf("Message format broken; %c is not a digit.\n", buff[4 + k]);
+						char error[4];
+						error[0] = 'M';
+						error[1] = i + '0';
+						error[2] = 'F';
+						error[3] = 'T';
+						write(connectionFileDesc, error, 4);
+						finished = 1;
+						break;
+					}
+					if (buff[4 + k] == '|') {
+						char *length = malloc((k + 1 + 1) * sizeof(char));
+						if (length == NULL) {
+							printf("Malloc error.\n");
+							return 1;
+						}
+						memcpy(length, &buff[4], k + 1);
+						length[k + 2] = '\0';
+						int intLength;
+						intLength = (int) strtol(length, NULL, 10);
+						if (4 + k + intLength >= size) {
+							size *= 2;
+							buff = realloc(buff, size);
+							if (buff == NULL) {
+								printf("Malloc error.\n");
+								return 1;
+							}
+						}
+						if (readXBytes(connectionFileDesc, &buff[4 + k + 1], intLength+1) == 1) {
+							printf("Early termination.\n");
+							finished = 1;
+						}
+						printf("Read in %.*s as a string\n", intLength+1, &buff[4+k+1]);
+						buff[4+k+1+intLength+2] = '\0';
+						free(length);
+						break;
+					}
+					k += 1;
+				}
+			} else if (strcmp("ERR|", buff) == 0) {
+				//need to read in all of the error message
+				int k = 0;
+				while (1) {
+					if (4 + k == size) {
+						size *= 2;
+						buff = realloc(buff, size);
+						if (buff == NULL) {
+							printf("Malloc error.\n");
+							return 1;
+						}
+					}
+					if (read(connectionFileDesc, &buff[5 + k], 1) == 0) {
+						printf("Early termination of client. \n");
+						finished = 1;
+						break;
+					}
+					if (buff[4 + k] == '|') {
+						buff[44 + k + 1] = '\0';
+						printf("Error received by client: '%s'\n", buff);
+						finished = 1;
+						break;
+					}
+					k += 1;
+				}
+			}
+			else
+			{
+				printf("Message format broken; %s is the buffer.\n", buff);
+				char error[4];
+				error[0] = 'M';
+				error[1] = i + '0';
+				error[2] = 'F';
+				error[3] = 'T';
+				write(connectionFileDesc, error, 4);
+				break;
+			}
+			char dummy[5] = "hello";
+
+			if (!finished) { checkMessage(i, buff, setUpLine, punchLine, dummy); }
 		}
+
+		close(connectionFileDesc);
+		free(buff);
 	}
 }
+
+int readXBytes(int socketFileDesc, char *buff, int x) {
+	int remaining = x;
+	while (remaining > 0) {
+		char c = '\0';
+		int nRead = read(socketFileDesc, &c, 1);
+		if (nRead == 0) {
+			printf("EOF reached\n");
+			return 1;
+		}
+		printf("%c read in readXBytes \n", c);
+		buff[x - remaining] = c;
+		remaining -= 1;
+	}
+	return 0;
+}
+
 //accepts stage of the joke and the input from client
 //returns "0" on success, error message on failure
 
-char *checkMessage(int stage, char *message, char* setUpLine, char* punchLine, char* adsLine);
-char* findError(int stage, char* message, char* expected, int expectedLen);
-int numDigits(int len);
-char* lengthAsString(int len);
-
-char *checkMessage(int stage, char *message, char* setUpLine, char* punchLine, char* adsLine) {
-	char* expected; 
-  int len;
-	switch (stage)
-	{
-	case 0:
-    len = 13;
-		expected = "REG|13|Knock, knock|"; 
-		break;
-	case 1: 
-    len= 12;
-		expected = "REG|12|Who's there?|";
-		break;
-  case 2: 
-    len = strlen(setUpLine);
-    expected = malloc((3 + 3 + numDigits(len) + len) * sizeof(char));
-    strcat(expected, "REG|");
-    strcat(expected, lengthAsString(len));
-    strcat(expected, "|");
-    strcat(expected, setUpLine);
-    strcat(expected, "|");
-    break;
-	case 3: 
-		len = strlen(setUpLine)+6;
-    expected = malloc((3 + 3 + numDigits(len) + len) * sizeof(char));
-    strcat(expected, "REG|");
-    strcat(expected, lengthAsString(len));
-    strcat(expected, "|");
-    strcat(expected, setUpLine);
-    strcat(expected, ", who?");
-    strcat(expected, "|");
-    break;
-  case 4: 
-    len = strlen(punchLine);
-    expected = malloc((3 + 3 + numDigits(len) + len) * sizeof(char));
-    strcat(expected, "REG|");
-    strcat(expected, lengthAsString(len));
-    strcat(expected, "|");
-    strcat(expected, punchLine);
-    strcat(expected, "|");
-    break;
-  case 5: 
-    len = strlen(adsLine);
-    expected = malloc((3 + 3 + numDigits(len) + len) * sizeof(char));
-    strcat(expected, "REG|");
-    strcat(expected, lengthAsString(len));
-    strcat(expected, "|");
-    strcat(expected, adsLine);
-    strcat(expected, "|");
-	default:
-		break;
+char *checkMessage(int stage, char *message, char *setUpLine, char *punchLine, char *adsLine) {
+	printf("Checking message %s at stage %d\n", message, stage);
+	char *expected;
+	int len;
+	switch (stage) {
+		case 0:
+			len = 13;
+			expected = "REG|13|Knock, knock.|";
+			break;
+		case 1:
+			len = 12;
+			expected = "REG|12|Who's there?|";
+			break;
+		case 2:
+			len = strlen(setUpLine);
+			expected = malloc((3 + 3 + numDigits(len) + len) * sizeof(char));
+			strcat(expected, "REG|");
+			strcat(expected, lengthAsString(len));
+			strcat(expected, "|");
+			strcat(expected, setUpLine);
+			strcat(expected, "|");
+			break;
+		case 3:
+			len = strlen(setUpLine) + 6;
+			expected = malloc((3 + 3 + numDigits(len) + len) * sizeof(char));
+			strcat(expected, "REG|");
+			strcat(expected, lengthAsString(len));
+			strcat(expected, "|");
+			strcat(expected, setUpLine);
+			strcat(expected, ", who?");
+			strcat(expected, "|");
+			break;
+		case 4:
+			len = strlen(punchLine);
+			expected = malloc((3 + 3 + numDigits(len) + len) * sizeof(char));
+			strcat(expected, "REG|");
+			strcat(expected, lengthAsString(len));
+			strcat(expected, "|");
+			strcat(expected, punchLine);
+			strcat(expected, "|");
+			break;
+		case 5:
+			len = strlen(adsLine);
+			expected = malloc((3 + 3 + numDigits(len) + len) * sizeof(char));
+			strcat(expected, "REG|");
+			strcat(expected, lengthAsString(len));
+			strcat(expected, "|");
+			strcat(expected, adsLine);
+			strcat(expected, "|");
+		default:
+			break;
 	}
 
-  if(strcmp(expected, message) != 0) return findError(stage, message, expected, len);
-	
+	if (strcmp(expected, message) != 0) return findError(stage, message, expected, len);
+
 	return NULL;
 }
 
 
-char* findError(int stage, char* message, char* expected, int expectedLen){
-  
-  char* err = malloc(5*sizeof(char));
-  err[0] = 'M';
-  err[1] = stage + 48;
-  err[2] = '\0';
+char *findError(int stage, char *message, char *expected, int expectedLen) {
+
+	char *err = malloc(5 * sizeof(char));
+	err[0] = 'M';
+	err[1] = stage + 48;
+	err[2] = '\0';
 
 
-  if(message[0] != expected[0] || message[1] != expected[1] || message[2] != expected[2] || message[3] != '|') 
-    return strcat(err, "FT");
+	if (message[0] != expected[0] || message[1] != expected[1] || message[2] != expected[2] || message[3] != '|')
+		return strcat(err, "FT");
 
-  int lenJoke = 0; 
+	int lenJoke = 0;
 
-  int i = 4;
-  int messageLen = strlen(message);
-  while( i < messageLen){
-    if(message[i] >= 48 && message[i] <= 57){
-      lenJoke = lenJoke* 10 + (message[i]-48);
-    }
-    else if(message[i] == '|'){
-      if(lenJoke == 0)
-        return strcat(err, "FT");
-      else break;
-    }
-    else{
-      return strcat(err, "FT"); 
-    }
-    i++;
-  }
+	int i = 4;
+	int messageLen = strlen(message);
+	while (i < messageLen) {
+		if (message[i] >= 48 && message[i] <= 57) {
+			lenJoke = lenJoke * 10 + (message[i] - 48);
+		} else if (message[i] == '|') {
+			if (lenJoke == 0)
+				return strcat(err, "FT");
+			else
+				break;
+		} else {
+			return strcat(err, "FT");
+		}
+		i++;
+	}
 
-  if(lenJoke != expectedLen) {
-    return strcat(err, "LN");
-  }
+	if (lenJoke != expectedLen) {
+		return strcat(err, "LN");
+	}
 
-  int start = i+1;
+	int start = i + 1;
 
-  for(i = start; i < start+lenJoke; i++){
-    if(message[i] == '|') return strcat(err, "FT");
-    if(message[i] != expected[i]) return strcat(err, "CT");
-  }
+	for (i = start; i < start + lenJoke; i++) {
+		if (message[i] == '|') return strcat(err, "FT");
+		if (message[i] != expected[i]) return strcat(err, "CT");
+	}
 
-  if(message[i] != '|') return strcat(err, "FT");
+	if (message[i] != '|') return strcat(err, "FT");
 
-  return strcat(err, "FT");
+	return strcat(err, "FT");
 }
 
 
 // char* findError(int stage, char* message, char* expected){
 
 //first check first three characters - must be REG
-//then check that there is a | 
+//then check that there is a |
 //then get length - must equal expected
-//then check that there is a | 
+//then check that there is a |
 //then read the content string
-	//if its length is not equal to read length - problem
-	//if its content not equal to expected - error
+//if its length is not equal to read length - problem
+//if its content not equal to expected - error
 //must have
-
-
 
 
 // }
 
 
-
-char* lengthAsString(int len){
-  int digits = numDigits(len);
-  char* res = malloc(digits* sizeof(char));
-  sprintf(res, "%d", len);
-  return res;
+char *lengthAsString(int len) {
+	int digits = numDigits(len);
+	char *res = malloc(digits * sizeof(char));
+	sprintf(res, "%d", len);
+	return res;
 }
 
-int numDigits(int len){
-  int res = 0; 
+int numDigits(int len) {
+	int res = 0;
 
-  while(len > 0){
-    res++;
-    len = len/10;
-  }
-  return res;
+	while (len > 0) {
+		res++;
+		len = len / 10;
+	}
+	return res;
 }
